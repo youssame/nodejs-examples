068aea43ddb63da96dc3ee2d560767f2d0ba4b32
Youssef AMEACHAQ <youssefameachaq@gmail.com>
2023-05-25 13:34:34 +0100

    setup: first commit

---
+++ src/file-system/index.ts
@@ -0,0 +1,12 @@
+import { readFile, writeFile } from 'fs/promises';
+let template = await readFile(new URL('./template.html', import.meta.url), 'utf-8')
+const data = {
+    title: 'My new file',
+    body: 'I wrote this file to disk using node'
+  }
+  
+  for (const [key, val] of Object.entries(data)) {
+    template = template.replace(`{${key}}`, val)
+  }
+  
+await writeFile(new URL('./index.html', import.meta.url), template)
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("*********************************************");
+console.log("This is some practices from a node.js courses");
+console.log("*********************************************");
